c445ea643661fc82f5f00abb328c6b56536ecf25
Frithjof Hoppe <121172321+frithjofhoppebit@users.noreply.github.com>
2024-04-27 17:06:49 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,10 +16,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(
+        .target(
             name: "BbsCryptoSuite",
-            url: "https://github.com/frithjofhoppebit/test-repo/raw/main/bbscryptosuite.xcframework.zip",
-            checksum:"53183a586b00db5255a5df31bc8afe3d"
-        )
+            dependencies: []
+        ),
     ]
 )
