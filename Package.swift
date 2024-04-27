300737b43832db5d97961419b3760eeeb73f93e0
Frithjof Hoppe <121172321+frithjofhoppebit@users.noreply.github.com>
2024-04-27 16:56:47 +0200

    Set checksum

--- Package.swift
+++ Package.swift
@@ -18,7 +18,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "BbsCryptoSuite",
-            url: "https://github.com/frithjofhoppebit/test-repo/raw/main/bbscryptosuite.xcframework.zip"
+            url: "https://github.com/frithjofhoppebit/test-repo/raw/main/bbscryptosuite.xcframework.zip",
+            checksum:"53183a586b00db5255a5df31bc8afe3d"
         )
     ]
 )
